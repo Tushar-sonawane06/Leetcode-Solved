3e1436992bbd4b33df935cf2caa7ff8677af6c53
Tushar Sonwane <tusharsonawane1903@gmail.com>
2026-08-19 14:46:15 +0530

    Time: 28 ms (17.28%) | Memory: 47.7 MB (43.34%) - LeetSync

---
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int minInsertions(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                stack.push(ch);
+            }
+            else {
+                if (i + 1 < s.length() && s.charAt(i + 1) == ')') {
+                    i++; 
+                } 
+                else {
+                    ans++;
+                }
+
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } 
+                else {
+                    ans++;
+                }
+            }
+        }
+        ans += stack.size() * 2;
+
+        return ans;
+    }
+}
