071c295269542a177ab851e7ccf7e5ba3a95832d
Tushar Sonwane <tusharsonawane1903@gmail.com>
2026-08-29 14:21:04 +0530

    Time: 10 ms (51.05%) | Memory: 59.1 MB (88.12%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -12,45 +12,45 @@ class Solution {
     public ListNode sortList(ListNode head) {
         if(head==null || head.next==null){
             return head;
-        }
+        }   
 
-        ListNode mid=getMid(head);
-        ListNode left=sortList(head);
-        ListNode right=sortList(mid);
+        ListNode mid = getMid(head);
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
 
         return merge(left,right);
     }
 
     public ListNode merge(ListNode list1, ListNode list2){
-        ListNode dummay = new ListNode();
-        ListNode tail=dummay;
+        ListNode dummy= new ListNode(0);
+        ListNode tail=dummy;
+
         while(list1!=null && list2!=null){
             if(list1.val<list2.val){
                 tail.next=list1;
                 list1=list1.next;
-                tail=tail.next;
             }else{
                 tail.next=list2;
                 list2=list2.next;
-                tail=tail.next;
             }
+            tail=tail.next;
         }
 
-        tail.next=(list1!=null) ? list1 : list2;
-        return dummay.next;
+        tail.next = (list1!=null) ? list1 : list2;
+
+        return dummy.next;
     }
 
     public ListNode getMid(ListNode head){
+        ListNode prev=null;
         ListNode slow=head;
         ListNode fast=head;
-        ListNode prev=null;
-
         while(fast!=null && fast.next!=null){
             prev=slow;
             slow=slow.next;
             fast=fast.next.next;
         }
-
         prev.next=null;
         return slow;
     }
