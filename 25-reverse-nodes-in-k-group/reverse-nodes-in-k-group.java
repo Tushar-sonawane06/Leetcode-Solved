309a04280437f01b5874185fef6ca75bc208351a
Tushar Sonwane <tusharsonawane1903@gmail.com>
2026-08-29 13:27:46 +0530

    Time: 1 ms (34.63%) | Memory: 46.5 MB (35.32%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -8,55 +8,40 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- class Solution {
+class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-
-        if (head == null || k <= 1) {
+        if(head==null || k==1){
             return head;
         }
 
-        ListNode curr = head;
-        ListNode prevGroupEnd = null;
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode groupPrev=dummy;
 
-        while (true) {
-
-            // Check if k nodes are available
-            ListNode temp = curr;
-
-            for (int i = 0; i < k; i++) {
-                if (temp == null) {
-                    return head;
+        while(true){
+            ListNode kth=groupPrev;
+            for(int i=0;i<k;i++){
+                kth=kth.next;
+                if(kth==null){
+                    return dummy.next;
                 }
-                temp = temp.next;
             }
 
-            // Save the start of this group
-            ListNode groupStart = curr;
+            ListNode groupNext=kth.next;
 
-            // Reverse this group
-            ListNode prev = null;
-            ListNode next;
+            ListNode prev=groupNext;
+            ListNode curr=groupPrev.next;
 
-            for (int i = 0; i < k; i++) {
-                next = curr.next;
-                curr.next = prev;
-                prev = curr;
-                curr = next;
+            while(curr!=groupNext){
+                ListNode next=curr.next;
+                curr.next=prev;
+                prev=curr;
+                curr=next;
             }
 
-            // Connect previous group to current reversed group
-            if (prevGroupEnd != null) {
-                prevGroupEnd.next = prev;
-            } else {
-                // First group becomes the new head
-                head = prev;
-            }
-
-            // groupStart is now the end of the reversed group
-            groupStart.next = curr;
-
-            // Update previous group end
-            prevGroupEnd = groupStart;
+            ListNode temp=groupPrev.next;
+            groupPrev.next=kth;
+            groupPrev=temp;
         }
     }
 }
